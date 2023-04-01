9758f6ad85c05f1d457990f0dc54c41bfe83c917
Tuomas Reunanen <tuomas.reunanen@proton.me>
2023-04-01 12:38:52 +0300

    Added a shutdown signal

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,13 @@
 use std::convert::Infallible;
 use std::error::Error;
+use std::future::Future;
 use std::net::SocketAddr;
 use std::path::Path;
+use std::pin::Pin;
+use std::thread;
 use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
+use std::time::Duration;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::body::HttpBody;
 use hyper::service::{make_service_fn, service_fn};
@@ -19,8 +24,10 @@ async fn process_request(req: Request<Body>, state: Arc<Mutex<SystemState>>) -> 
             *response.body_mut() = Body::from(format!("{status:?}"));
         },
         (&Method::POST, "/shutdown") => {
-            let mut state = state.lock().unwrap();
-            state.status = Status::Exiting;
+            {
+                let mut state = state.lock().unwrap();
+                state.status = Status::Exiting;
+            }
         },
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
@@ -34,6 +41,25 @@ async fn process_request(req: Request<Body>, state: Arc<Mutex<SystemState>>) -> 
 async fn main() -> Result<(), Box<dyn Error>> {
     let config = read_config(Path::new("./config.toml"))?;
     let state = Arc::new(Mutex::new(SystemState::new()));
+    // Initialize a shutdown signal
+    let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
+
+    let runner_state = state.clone();
+    thread::spawn(move || {
+        loop {
+            {
+                let mut state = runner_state.lock().unwrap();
+                let status = state.status;
+                if status == Status::Exiting {
+                    break;
+                }
+            }
+            thread::sleep(Duration::from_millis(10))
+        }
+
+        println!("Exiting server");
+        shutdown_tx.send(())
+    });
 
     let addr = SocketAddr::from(([127, 0, 0, 1], config.server.port));
 
@@ -43,10 +69,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
         async move { Ok::<_, Infallible>(service) }
     });
 
-    let server = Server::bind(&addr).serve(make_service).await;
+    let server = Server::bind(&addr)
+        .serve(make_service)
+        .with_graceful_shutdown(async {
+            shutdown_rx.await.ok();
+        })
+        .await;
 
     if let Err(e) = server {
-        eprintln!("server error: {}", e);
+        eprintln!("Unexpected error in server: {}", e);
     }
 
     Ok(())
