79d8ada81a666358a37e7c88b67686453912e0b3
Tuomas Reunanen <tuomas.reunanen@proton.me>
2023-04-01 10:50:55 +0300

    Began implementing UI

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,68 @@
-use model::config::ServiceConfig;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
+use model::config::{ServiceConfig, read_config};
+use std::{
+    thread,
+    fs::File,
+    error::Error,
+    io::{BufReader, stdout, Result as IOResult},
+    path::Path,
+    time::Duration
+};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::style::{Color, Style};
 
-fn main() {
-    let config = read_config(Path::new("../sampleConfig.yml"));
-    println!("Hello, config: {config:?}");
-}
+fn main() -> IOResult<()> {
+    let config = read_config(Path::new("./sampleConfig.yml"));
 
-fn read_config<P: AsRef<Path>>(path: P) -> Result<ServiceConfig, Box<dyn Error>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
+    enable_raw_mode()?;
 
-    Ok(serde_yaml::from_reader(reader)?)
+    let stdout = stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    let mut executing = true;
+
+    while executing {
+        terminal.draw(|f| {
+            let size = f.size();
+
+            let block = Block::default()
+                .style(
+                    Style::default()
+                        .bg(Color::Black)
+                ).title(format!("Block"))
+                .borders(Borders::ALL);
+            f.render_widget(block, size);
+        })?;
+
+        let has_event = event::poll(Duration::from_millis(20))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Esc => {
+                    executing = false;
+                },
+                _ => {}
+            }
+        }
+    }
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
--- shared/src/config.rs
+++ shared/src/config.rs
@@ -1,4 +1,8 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 use serde::Deserialize;
 use Vec;
 
@@ -14,4 +18,11 @@ pub struct Service {
     pub depends_on: Vec<String>,
     pub compile: Option<String>,
     pub run: String
+}
+
+pub fn read_config<P: AsRef<Path>>(path: P) -> Result<ServiceConfig, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    Ok(serde_yaml::from_reader(reader)?)
 }
