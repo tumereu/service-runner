890fb5255f0145ad9c1d36c7148a82c224204504
Tuomas Reunanen <tuomas.reunanen@proton.me>
2023-04-09 11:00:23 +0300

    Moved size inside mod.rs

--- client/src/ui/widgets/mod.rs
+++ client/src/ui/widgets/mod.rs
@@ -1,18 +1,36 @@
-
-
+use std::cmp::min;
 use tui::backend::Backend;
 use tui::Frame;
 use tui::layout::Rect;
 
-
-
 pub use flex::*;
 pub use list::*;
-pub use size::*;
 
-mod size;
 mod flex;
 mod list;
+
+#[derive(Clone, Copy)]
+pub struct Size {
+    pub width: u16,
+    pub height: u16
+}
+impl Size {
+    pub fn intersect(&self, other: Size) -> Size {
+        (
+            min(self.width, other.width),
+            min(self.height, other.height)
+        ).into()
+    }
+}
+
+impl<X : Into<u16>, Y : Into<u16>> From<(X, Y)> for Size {
+    fn from(value: (X, Y)) -> Self {
+        Size {
+            width: value.0.into(),
+            height: value.1.into()
+        }
+    }
+}
 
 pub fn render_root<B, R>(root: R, frame: &mut Frame<B>) where B : Backend, R: Into<Renderable> {
     root.into().render(frame.size(), frame);
